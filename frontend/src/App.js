ed57fcb23265c75e6e61fc17f78e8a758aa639f4
Gabriela Ibarra <gabriela.estefania.ibarra@gmail.com>
2022-08-10 16:18:02 -0500

    Conditionally render header with useLocation hook

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { useLocation } from 'react-router';
+import { Routes, Route } from 'react-router-dom';
 import Home from './Pages/Home';
 import Trip from './Pages/Trip';
 import TripProposal from './Pages/TripProposal';
@@ -11,24 +12,19 @@ export const url = process.env.REACT_APP_DEV_SERVER_URL;
 
 function App() {
   const [userData, setUserData] = useState({});
+  let location = useLocation();
   return (
-    <Router>
-      {/* Components added within Router component will be displayed on every page */}
-      <div className="main-page">
-        <div className="main-container">
-          {userData.id && <Header />}
-          <Routes>
-            <Route
-              path="/"
-              element={<SignInSide setUserData={setUserData} />}
-            />
-            <Route path="/user/:userID" element={<Home />} />
-            <Route path="/user/:userID/trip/:tripID" element={<Trip />} />
-            <Route path="/proposal/:id" element={<TripProposal />} />
-          </Routes>
-        </div>
+    <div className="main-page">
+      <div className="main-container">
+        {location.pathname !== '/' && <Header />}
+        <Routes>
+          <Route path="/" element={<SignInSide setUserData={setUserData} />} />
+          <Route path="/user/:userID" element={<Home />} />
+          <Route path="/user/:userID/trip/:tripID" element={<Trip />} />
+          <Route path="/proposal/:id" element={<TripProposal />} />
+        </Routes>
       </div>
-    </Router>
+    </div>
   );
 }
 
