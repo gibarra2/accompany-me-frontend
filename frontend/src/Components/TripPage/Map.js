f90c4a6d2c12ddf2d4f0cf5a9c76e0e3b253fb6d
Gabriela Ibarra <gabriela.estefania.ibarra@gmail.com>
2022-08-11 19:04:59 -0500

    Center of map changes so all markers are visible

--- frontend/src/Components/TripPage/Map.js
+++ frontend/src/Components/TripPage/Map.js
@@ -2,6 +2,28 @@ import React from 'react';
 import GoogleMapReact from 'google-map-react';
 import PlaceMarker from './PlaceMarker';
 
+const getMax = (arr, property) => {
+  // Get the max value for a specific property of objects in an array
+  return Math.max(...arr.map((item) => Number(item[property])));
+};
+
+const getMin = (arr, property) => {
+  // Get the max value for a specific property of objects in an array
+  return Math.min(...arr.map((item) => Number(item[property])));
+};
+
+const getCenter = (placesArr) => {
+  /*
+  Finds midpoint of long and lat. 
+  */
+  const centerCoord = {};
+  centerCoord.lat =
+    (getMax(placesArr, 'latitude') + getMin(placesArr, 'latitude')) / 2;
+  centerCoord.lng =
+    (getMax(placesArr, 'longitude') + getMin(placesArr, 'longitude')) / 2;
+  return centerCoord;
+};
+
 const Map = ({ cityLat, cityLng, places }) => {
   if (!places || !cityLat) {
     return <></>;
@@ -14,6 +36,8 @@ const Map = ({ cityLat, cityLng, places }) => {
     },
     zoom: 11,
   };
+
+  const newCenter = places.length ? getCenter(places) : defaultProps.center;
 
   const allPlaceMarkers = (placesArr) => {
     return placesArr?.map((place) => {
@@ -39,6 +63,8 @@ const Map = ({ cityLat, cityLng, places }) => {
         bootstrapURLKeys={{ key: process.env.REACT_APP_G_MAPS_API_KEY }}
         defaultCenter={defaultProps.center}
         defaultZoom={defaultProps.zoom}
+        center={newCenter}
+        margin={[50, 50, 50, 50]}
       >
         {allPlaceMarkers(places)}
       </GoogleMapReact>
